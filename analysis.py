a5709938c90e2ad090f1b93720d84082ab9402b4
Grace Smyth <gracemurphy45@gmail.com>
2024-05-09 11:01:40 +0100

    Importing Libraries that may be used in this project

---
+++ analysis.py
@@ -0,0 +1,11 @@
+# Analysis of Fisher’s Iris data set.
+# Project for Programming and scripting module, GMIT 2018.
+# Lecturer: Andrew Beatty
+# Author: Grace Mary Smyth
+
+# Importing the necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
